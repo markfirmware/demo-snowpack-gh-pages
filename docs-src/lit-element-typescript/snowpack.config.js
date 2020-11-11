b3a2ae39f8133ed5211304449dab001b5d521ca4
markfirmware <mark.kendrat@gmail.com>
2020-11-11 18:22:00 +0000

    docs-src/lit-element-typescript

---
+++ docs-src/lit-element-typescript/snowpack.config.js
@@ -0,0 +1,32 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: [
+    '@snowpack/plugin-babel',
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-typescript',
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    baseUrl: '/demo-snowpack-gh-pages/lit-element-typescript',
+    out: '../../docs/lit-element-typescript'
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
