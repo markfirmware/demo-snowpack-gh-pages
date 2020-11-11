ff7b06a5ecaeb886d113612fa7d3919d4ab249e7
markfirmware <mark.kendrat@gmail.com>
2020-11-11 18:24:48 +0000

    docs-src/vue-typescript

---
+++ docs-src/vue-typescript/snowpack.config.js
@@ -0,0 +1,32 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: [
+    '@snowpack/plugin-vue',
+    '@snowpack/plugin-vue/plugin-tsx-jsx.js',
+    '@snowpack/plugin-dotenv',
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    baseUrl: '/demo-snowpack-gh-pages/vue-typescript',
+    out: '../../docs/vue-typescript'
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
