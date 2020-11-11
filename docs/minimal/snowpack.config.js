b3db3a655a48a2a90307ff3f8ab2a0555278ee06
markfirmware <mark.kendrat@gmail.com>
2020-11-11 18:22:15 +0000

    npm run build -> docs/minimal

---
+++ docs/minimal/snowpack.config.js
@@ -0,0 +1,29 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    /* ... */
+  },
+  plugins: [
+    /* ... */
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    baseUrl: '/demo-snowpack-gh-pages/minimal',
+    out: '../../docs/minimal'
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
