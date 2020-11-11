1614690abb96daa99b316d3187a9b5f2b169d46e
markfirmware <mark.kendrat@gmail.com>
2020-11-11 18:23:58 +0000

    docs-src/svelte

---
+++ docs-src/svelte/snowpack.config.js
@@ -0,0 +1,31 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: [
+    '@snowpack/plugin-svelte',
+    '@snowpack/plugin-dotenv',
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    baseUrl: '/demo-snowpack-gh-pages/svelte',
+    out: '../../docs/svelte'
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
