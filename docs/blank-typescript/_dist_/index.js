244ddb0a3b4fddf88de961e0df75f56b68dce4f3
markfirmware <mark.kendrat@gmail.com>
2020-11-11 18:21:40 +0000

    npm run build -> docs/blank-typescript

---
+++ docs/blank-typescript/_dist_/index.js
@@ -0,0 +1,5 @@
+import confetti from "../web_modules/canvas-confetti.js";
+confetti.create(document.getElementById("canvas"), {
+  resize: true,
+  useWorker: true
+})({particleCount: 200, spread: 200});
